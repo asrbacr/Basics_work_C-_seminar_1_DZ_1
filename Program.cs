5036adcbeb32bf6f957b625a4dafd5ec87502675
Vasiliy Gorilev <89159793029@ya.ru>
2023-01-16 12:29:46 +0300

    add task 1 and comment

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// Console.WriteLine("Задача на проверку большего числа среди 2-ух");
+// Console.Write("Введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if (a > b)
+// {
+//     Console.WriteLine("max = " + a);
+// }
+// else if (b > a)
+// {
+//     Console.WriteLine("max = " + b);
+// }
+// else
+// {
+//     Console.WriteLine("Числа равны");
+// }
+
